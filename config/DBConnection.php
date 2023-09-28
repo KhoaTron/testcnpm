5bbb2471ec6a2292c0f8534086a7d825de2f390b
Khoa <khoadang3211@gmail.com>
2023-09-29 00:07:25 +0700

    bai thuc hanh php

---
+++ config/DBConnection.php
@@ -0,0 +1,26 @@
+<?php
+class DBConnection{
+    private $conn=null;
+
+    public function __construct(){
+        // B1. Kết nối DB Server
+        try {
+            $this->conn = new PDO('mysql:host=localhost;dbname=btth02_cse485;port=3306', 'root','');
+        } 
+        catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    public function getConnection(){
+        return $this->conn;
+    }
+    public function runSql(string $sql,array $arguments=null)
+    {
+        if (!$arguments) {
+            return $this->conn->query($sql);
+        }
+        $statement = $this->conn->prepare($sql);
+        $statement->execute($arguments);
+        return $statement;
+    }
+}
